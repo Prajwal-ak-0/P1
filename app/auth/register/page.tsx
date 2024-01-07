fd62a0f7058ccf891e396e0301941c9dbf171f17
Prajwal A K <prajwalhassan7@gmail.com>
2024-01-07 21:25:09 +0530

    Login and Register Layout Complted!

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,11 @@
+import { RegisterForm } from "@/components/Auth/RegisterForm"
+
+const LoginPage = () => {
+    return (
+        <div>
+            <RegisterForm/>
+        </div>
+    )
+}
+
+export default LoginPage
