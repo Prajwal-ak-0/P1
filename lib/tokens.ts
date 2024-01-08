5f70760654fbc1d42ced52a832102b56533510d9
Prajwal A K <prajwalhassan7@gmail.com>
2024-01-08 23:52:18 +0530

    Password Rest Completed

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,6 +1,7 @@
 import {v4 as uuidv4} from 'uuid';
 import { getVerificationTokenByEmail } from './getVerificationToken';
 import { db } from './db';
+import { getPasswordResetTokenByEmail } from '@/data/PasswordResetToken';
 
 export const generateVerificationToken = async (email: string) => {
     const token = uuidv4();
@@ -26,3 +27,26 @@ export const generateVerificationToken = async (email: string) => {
   
     return verficationToken;
   };
+
+  export const generatePasswordResetToken = async (email: string) => {
+    const token = uuidv4();
+    const expires = new Date(new Date().getTime() + 3600 * 1000);
+  
+    const existingToken = await getPasswordResetTokenByEmail(email);
+  
+    if (existingToken) {
+      await db.passwordResetToken.delete({
+        where: { id: existingToken.id }
+      });
+    }
+  
+    const passwordResetToken = await db.passwordResetToken.create({
+      data: {
+        email,
+        token,
+        expires
+      }
+    });
+  
+    return passwordResetToken;
+  }
