3a65d1dea4b4b51b2fd670de7bd38346d3de4db1
Prajwal A K <prajwalhassan7@gmail.com>
2024-01-09 19:16:59 +0530

    Able to access the session info on Client side and as well server side

---
+++ app/(protected)/server/page.tsx
@@ -0,0 +1,17 @@
+import { auth } from '@/auth'
+import { UserInfo } from '@/components/UserInfo';
+import { currentUser } from '@/lib/auth'
+import React from 'react'
+
+const page = async () => {
+    const user = await currentUser();
+
+  return (
+    <UserInfo
+      label="💻 Server component"
+      user={user}
+    />
+  )
+}
+
+export default page
--- auth.ts
+++ auth.ts
@@ -60,6 +60,10 @@ export const {
                 session.user.role = token.role as UserRole
             }
 
+            if (session.user) {
+                session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+            }
+
             return session;
         },
         async jwt({ token }) {
@@ -69,7 +73,8 @@ export const {
 
             if (!existingUser) return token;
 
-            token.role = existingUser.role
+            token.role = existingUser.role;
+            token.isTwoFactorEnabled = existingUser.isTwoFactorEnabled;
             return token;
         }
     },
